197e8dd6b408c22fa7455e5ddf24c242728ff992
楊承恩 <10411222@gm.nttu.edu.tw>
2020-11-11 09:15:02 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Fragment } from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>test message</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>hello</h1>
+    </Fragment>
   );
 }
 
